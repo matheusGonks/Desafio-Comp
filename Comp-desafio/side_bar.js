b0e55412e38cc1f162248083c65cee566b4c517b
matheusGonks <90430958+matheusGonks@users.noreply.github.com>
2022-06-22 17:50:46 -0300

    Add files via upload

---
+++ Comp-desafio/side_bar.js
@@ -0,0 +1,7 @@
+function side_open() {
+    document.getElementById("side-bar").style.display = "block";
+}
+
+function side_close() {
+    document.getElementById("side-bar").style.display = "none";
+}
